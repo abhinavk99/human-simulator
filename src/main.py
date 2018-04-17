1d63abb8fcb2b11d2604c3e72002abde2126b2da
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-04-17 17:51:26 -0500

    Set up connection to Twitter

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-from writer import TwitterWriter
+from writer import TwitterWriter, Tokenization
+import tweepy
+import config
+
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_secret)
+
+api = tweepy.API(auth)
+
+TwitterWriter writer = TwitterWriter(5, Tokenization.character)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)

2ea1d44992273bf609f10912f49484adb3b082ed
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-04-17 18:03:38 -0500

    Bot tweets random output

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 from writer import TwitterWriter, Tokenization
 import tweepy
 import config
+import itertools
+import re
 
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 auth.set_access_token(config.access_token, config.access_secret)
 
 api = tweepy.API(auth)
 
-TwitterWriter writer = TwitterWriter(5, Tokenization.character)
+writer = TwitterWriter(50, Tokenization.character)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+for tweet in api.home_timeline():
+    # Remove all links from the text using regex and newlines
+    text = re.sub(r'http\S+', '', tweet.text)
+    text = text.replace('\n', '')
+    writer.learn_iterable(text)
+
+out_tweet = ''.join(itertools.islice(writer.output(), 200))
+api.update_status(out_tweet)
